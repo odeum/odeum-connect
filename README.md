277ac45c489aef179957bd79f3039897efabd7fb
Christian Broberg <cb@webhouse.dk>
2017-04-11 01:43:21 +0200

    README.md update

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ODEUM Connect is a module that connects ODEUM CodeJS with ODEUM API.
 
-```
+```sh
 npm install odeum-connect
+```
+
+## Usage
+
+```javascript
+import React from 'react'
+
+...
+
+<APIConnector />
 ```
--- src/index.js
+++ src/index.js
@@ -2,72 +2,8 @@ import React from 'react'
 
 const APIConnector = () => {
   return (
-    <div>ODEUM Connect is a module that connects ODEUM CodeJS with ODEUM API v. 0.1.0</div>
+    <div>ODEUM CodeJS is now connected to ODEUM API (module v. 0.1.1) ... </div>
   )
 }
 
 export default APIConnector
-
-export class GeoLocation extends React.Component {
-
-    constructor(props) {
-      super(props);
-     
-      this.state = {
-        lat:  '',
-        long: ''
-      }
-
-      this.GetLatitude = this.GetLatitude.bind(this)
-      this.GetLongitude = this.GetLongitude.bind(this)
-      this.GetLatitude()
-      this.GetLongitude()
-    }
-
-    GetLatitude() {
-
-    var _this = this
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude
-            _this.setState({lat});        
-        })
-    }
-
-    else {
-
-    console.log('Geolocation is not available')
-    }
-
-    }
-
-    GetLongitude() {
-
-    var _this = this;
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var long = position.coords.longitude
-            _this.setState({long});        
-        })
-    }
-
-    else {
-
-    console.log('Geolocation is not available')
-    }
-
-    }
-
-    render() {
-        
-        return (
-            <div>
-                <p>{ this.state.lat } </p>
-                <p>{ this.state.long } </p>
-            </div>
-        );
-    }
-}
-
