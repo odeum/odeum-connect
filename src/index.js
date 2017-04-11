5c561c23decf25c44f1e78a296310d3f13609256
Christian Broberg <cb@webhouse.dk>
2017-04-11 02:00:13 +0200

    Travis tests

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const APIConnector = () => {
   return (
-    <div>ODEUM CodeJS is now connected to ODEUM API (module v. 0.1.1) ... </div>
+    <div>ODEUM CodeJS is now connected to ODEUM API (module v. 0.1.2) ... </div>
   )
 }
 
